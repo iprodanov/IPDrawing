d61beabde04cd727cecee385680cc3b25d0da984
Ivan Prodanov <nsu_smile@abv.bg>
2012-12-05 17:26:01 +0200

    Update IPDrawing/IPDrawing/Source/Model/Shape.cs
    Identation problems

--- IPDrawing/IPDrawing/Source/Model/Shape.cs
+++ IPDrawing/IPDrawing/Source/Model/Shape.cs
@@ -22,35 +22,35 @@ namespace Draw
         #region Constructors
 
         public Shape()
-		{
-		}
+	{
+	}
 		
-		public Shape(RectangleF rect)
-		{
-			this.LineColor = Color.Black;
-            this.FillColor = Color.White;
-            this.LineWidth = 2;
-            this.Transparency = 255;
-            this.Rectangle = rect;
-            this.TransformMatrix = new ShapeMatrix();
-		}
+	public Shape(RectangleF rect)
+	{
+		this.LineColor = Color.Black;
+		this.FillColor = Color.White;
+		this.LineWidth = 2;
+		this.Transparency = 255;
+		this.Rectangle = rect;
+		this.TransformMatrix = new ShapeMatrix();
+	}
 
-		public Shape(Shape shape)
-		{
-			this.Height = shape.Height;
-			this.Width = shape.Width;
-			this.Location = shape.Location;
-			this.Rectangle = shape.rectangle;
+	public Shape(Shape shape)
+	{
+		this.Height = shape.Height;
+		this.Width = shape.Width;
+		this.Location = shape.Location;
+		this.Rectangle = shape.rectangle;
 			
-			this.FillColor =  shape.FillColor;
-            this.LineColor = shape.LineColor;
-            this.LineWidth = shape.LineWidth;
-            this.TransformMatrix = shape.TransformMatrix;
-		}
+		this.FillColor =  shape.FillColor;
+		this.LineColor = shape.LineColor;
+		this.LineWidth = shape.LineWidth;
+		this.TransformMatrix = shape.TransformMatrix;
+	}
 		
         #endregion
 		
-		#region Properties
+	#region Properties
         
         public int ID
         {
@@ -58,124 +58,124 @@ namespace Draw
             set { id = value; }
         }
 		
-		public virtual RectangleF Rectangle
+	public virtual RectangleF Rectangle
         {
-			get { return rectangle; }
-			set { rectangle = value; }
-		}
+		get { return rectangle; }
+		set { rectangle = value; }
+	}
 		
-		public virtual float Width
+	public virtual float Width
         {
-			get { return Rectangle.Width; }
-			set 
-            {
-                if (value > 0)
-                {
-                    rectangle.Width = value;
-                }
-            }
-		}
+		get { return Rectangle.Width; }
+		set 
+		{
+                	if (value > 0)
+			{
+                    		rectangle.Width = value;
+                	}
+            	}
+	}
 		
-		public virtual float Height
+	public virtual float Height
         {
-			get { return Rectangle.Height; }
-			set
-            {
-                if (value > 0)
-                {
-                    rectangle.Height = value;
-                }
-            }
-		}
+		get { return Rectangle.Height; }
+		set
+            	{
+                	if (value > 0)
+                	{
+                    		rectangle.Height = value;
+                	}
+            	}
+	}
 		
-		public virtual PointF Location
+	public virtual PointF Location
         {
-			get { return Rectangle.Location; }
-			set { rectangle.Location = value; }
-		}
+		get { return Rectangle.Location; }
+		set { rectangle.Location = value; }
+	}
 		
-		public virtual Color FillColor
+	public virtual Color FillColor
         {
-			get { return fillColor; }
-			set { fillColor = value; }
-		}
+		get { return fillColor; }
+		set { fillColor = value; }
+	}
 
         public virtual Color LineColor
         {
-            get { return lineColor; }
-            set { lineColor = value; }
+		get { return lineColor; }
+		set { lineColor = value; }
         }
 
         public virtual int LineWidth
         {
-            get { return lineWidth; }
-            set 
-            {
-                if (value > 0)
-                {
-                    lineWidth = value;
-                }
-            }
+		get { return lineWidth; }
+		set 
+		{
+			if (value > 0)
+			{
+				lineWidth = value;
+			}
+		}
         }
 
         public virtual int Transparency
         {
-            get { return transparency; }
-            set
-            {
-                if (value >= 0 && value <= 255)
-                {
-                    transparency = value;
-                }
-            }
+        	get { return transparency; }
+		set
+		{
+			if (value >= 0 && value <= 255)
+			{
+				transparency = value;
+			}
+		}
         }
 
         public virtual ShapeMatrix TransformMatrix
         {
-            get { return transformMatrix; }
-            set { transformMatrix = value; }
+		get { return transformMatrix; }
+	        set { transformMatrix = value; }
         }
 
-		#endregion
+	#endregion
 
         #region Methods
 
         public virtual PointF GetCenter()
         {
-            float ofsetX = this.Width / 2;
-            float ofsetY = this.Height / 2;
-            PointF center = new PointF(this.Location.X + ofsetX, this.Location.Y + ofsetY);
+            	float ofsetX = this.Width / 2;
+            	float ofsetY = this.Height / 2;
+            	PointF center = new PointF(this.Location.X + ofsetX, this.Location.Y + ofsetY);
             
-            return center;
+            	return center;
         }
 
         public virtual PointF[] GetCoveringRectanglePoints()
         { 
-            PointF[] points = 
-                {
-                    new PointF (this.Rectangle.X, this.Rectangle.Y),
-                    new PointF (this.Rectangle.X + this.Width, this.Rectangle.Y),
-                    new PointF (this.Rectangle.X, this.Rectangle.Y + this.Height),
-                    new PointF (this.Rectangle.X + this.Width, this.Rectangle.Y + this.Height)
-                };
-            this.TransformMatrix.TransformPoints(points);
+		PointF[] points = 
+                	{
+	                    	new PointF (this.Rectangle.X, this.Rectangle.Y),
+        	            	new PointF (this.Rectangle.X + this.Width, this.Rectangle.Y),
+                	    	new PointF (this.Rectangle.X, this.Rectangle.Y + this.Height),
+                    		new PointF (this.Rectangle.X + this.Width, this.Rectangle.Y + this.Height)
+                	};
+            	this.TransformMatrix.TransformPoints(points);
             
-            return points;
+            	return points;
         }
 
         public bool IsGroup()
         {
-            return (this.GetType().ToString() == "Draw.GroupShape");
+            	return (this.GetType().ToString() == "Draw.GroupShape");
         }
 
         public virtual bool Contains(PointF point)
-		{
-            RectangleF rect = Rectangle;
-            rect.Inflate(LineWidth / 2, LineWidth / 2);
-            return rect.Contains(point);
-		}
+	{
+		RectangleF rect = Rectangle;
+            	rect.Inflate(LineWidth / 2, LineWidth / 2);
+            	return rect.Contains(point);
+	}
 		
-		public abstract void DrawSelf(Graphics grfx);
+	public abstract void DrawSelf(Graphics grfx);
 
         #endregion
     }
