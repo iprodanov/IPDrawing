058f14bce99c6a02adbd49c131ee1432d34904eb
iprodanov <iprodanov@gmail.com>
2012-12-05 18:02:52 +0200

    Correct identations
    Identations Problems

--- IPDrawing/IPDrawing/Source/Model/RectangleShape.cs
+++ IPDrawing/IPDrawing/Source/Model/RectangleShape.cs
@@ -5,25 +5,24 @@ namespace Draw
 {
 	[Serializable]
     public class RectangleShape : Shape
-	{
-		#region Constructors
-		
-		public RectangleShape(RectangleF rect)
+    {
+        #region Constructors
+        public RectangleShape(RectangleF rect)
             : base(rect)
         {
             this.FillColor = Color.FromArgb(0, 153, 255);
         }
-		
-		public RectangleShape(RectangleShape rectangle)
+        
+        public RectangleShape(RectangleShape rectangle)
             : base(rectangle)
-		{ }
-		
-		#endregion
-
+        { }
+        
+        #endregion
+        
         #region Methods
-
+        
         public override bool Contains(PointF point)
-		{
+        {
             TransformMatrix.InverseTransformPoint(ref point);
             if (base.Contains(point))
             {
@@ -33,16 +32,16 @@ namespace Draw
             {
                 return false;
             }
-		}
-		
-		public override void DrawSelf(Graphics grfx)
-		{
+        }
+        
+        public override void DrawSelf(Graphics grfx)
+        {
             grfx.Transform = this.TransformMatrix.GetMatrix();
             grfx.FillRectangle(new SolidBrush(Color.FromArgb(Transparency, FillColor)), Rectangle);
-			grfx.DrawRectangle(new Pen(LineColor, LineWidth), Rectangle.X, Rectangle.Y, Rectangle.Width, Rectangle.Height);
+            grfx.DrawRectangle(new Pen(LineColor, LineWidth), Rectangle.X, Rectangle.Y, Rectangle.Width, Rectangle.Height);
             grfx.Transform.Reset();
         }
-
+        
         #endregion
     }
 }
